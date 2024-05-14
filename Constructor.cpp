f7cc994c3d7e44ffac35001f6d030443f17a6ccb
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-05-14 14:50:00 +0700

    membuat mhs printAll

--- Constructor.cpp
+++ Constructor.cpp
@@ -42,5 +42,11 @@ int main()
     mahasiswa mhs3("Andi Kurniawan");
     mahasiswa mhs4("Joko Purbo");
 
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+    return 0;
+
 }
 
