f484bb9acc78b08344bfec72db3f52f48b720814
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-05-14 14:47:50 +0700

    Mengisi data mahasiswa

--- Constructor.cpp
+++ Constructor.cpp
@@ -35,3 +35,12 @@ void mahasiswa:: printAll()
     cout << endl;
 }
 
+int main()
+{
+    mahasiswa mhs1("Lia Kurnia");
+    mahasiswa mhs2("Asroni");
+    mahasiswa mhs3("Andi Kurniawan");
+    mahasiswa mhs4("Joko Purbo");
+
+}
+
