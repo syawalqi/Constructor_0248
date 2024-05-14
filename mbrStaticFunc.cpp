02c8d55ae0c68c11fd0ee069aa32ce3fec0c561b
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-05-14 15:01:52 +0700

    membuat panggilan nama

--- mbrStaticFunc.cpp
+++ mbrStaticFunc.cpp
@@ -16,5 +16,11 @@ class mahasiswa
 
     static void setNim(int pNim) { nim = pNim;}
     static int getNim() {return nim;}
+
+    mahasiswa(string pnama)
+    {
+        nama = pnama;
+        setID();
+    }
 };
 
